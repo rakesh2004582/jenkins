f4c9834b671deff61874fbf1e4e95bb1a8aff70d
rakesh kumar <rakeshkumarshakya433@gmail.com>
2025-02-28 14:33:36 +0530

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+
+hamburger = document.querySelector(".hamburger");
+// console.log("hamburger = "+hamburger);
+// hamburger.onclick = function(){
+//     navBar = document.querySelector(".nav-bar");
+//     navBar.classList.toggle("active");
+//     console.log("navBar = "+navBar)
+// }
+
+function showMenu(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+    console.log("navBar = "+navBar)
+}
